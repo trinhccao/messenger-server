ba4c293a4ca1a86de2e66fefda9765db923cd3e7
Trinh <t.trinh.it@gmail.com>
2023-04-04 17:18:05 +0700

    Login using JWT

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+import User from '../models/User'
 
 const authController = {
   authenticate: async (req: Request, res: Response, next: NextFunction) => {
@@ -7,7 +9,18 @@ const authController = {
   },
 
   login: async (req: Request, res: Response) => {
-    res.send('Login')
+    try {
+      const { username, password } = req.body
+      const user = await User.findOne({ username, password }, { password: 0 })
+      if (!user) {
+        return res.sendStatus(401)
+      }
+      const secret = process.env.JWT_SECRET as string
+      const token = jwt.sign({ user: user.toObject() }, secret)
+      res.json({ token: token, tokenType: 'Bearer' })
+    } catch (err) {
+      res.sendStatus(401)
+    }
   }
 }
 
