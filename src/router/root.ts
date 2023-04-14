5abff25a6f9377c3f3dc646ef93a943e9937690f
Trinh <t.trinh.it@gmail.com>
2023-04-14 16:15:25 +0700

    Update root.ts

--- src/router/root.ts
+++ src/router/root.ts
@@ -1,12 +1,12 @@
 import express from 'express'
 import user from './user'
-import authRouter from './auth'
+import auth from './auth'
 import thread from './thread'
 import message from './message'
 
 const router = express.Router()
 
-router.use(authRouter)
+router.use(auth)
 router.use('/users', user)
 router.use('/threads', thread)
 router.use('/messages', message)
