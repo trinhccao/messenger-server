e772483e937472b459ab7616162429c79d94b0fe
Trinh <t.trinh.it@gmail.com>
2023-04-16 22:48:17 +0700

    Create new thread if not exist

--- src/controllers/chat-controller.ts
+++ src/controllers/chat-controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express'
-import Thread, { ThreadTypes } from '../models/Thread'
+import Thread, { ThreadScopes, ThreadTypes } from '../models/Thread'
 import { AuthorizedRequest } from '../interfaces/AuthorizedRequest'
+import User from '../models/User'
+import { toDataThread } from '../helpers/thread-helper'
 
 const chatController = {
   findThreadId: async (req: Request, res: Response) => {
@@ -16,10 +18,23 @@ const chatController = {
           }
         ]
       })
-      if (!thread) {
+      if (thread) {
+        return res.send(thread.id)
+      }
+      const user = await User.findById(slug)
+      if (!user) {
         return res.sendStatus(404)
       }
-      res.send(thread.id)
+      const now = Date.now()
+      const newThread = await Thread.create({
+        members: [client._id, user._id],
+        createdAt: now,
+        updatedAt: now,
+        type: ThreadTypes.Direct,
+        scopes: [ThreadScopes.Member],
+      })
+      const convert = await toDataThread(newThread, client._id)
+      res.send(convert)
     } catch (err) {
       res.sendStatus(400)
     }
